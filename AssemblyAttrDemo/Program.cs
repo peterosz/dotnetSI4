5851b14b5ddc9ad5cdf58f38080cdadd87c81c88
peter.osz <itharius@gmail.com>
2017-12-07 16:46:18 +0100

    Exercise 2 done

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Assembly a = Assembly.GetExecutingAssembly();
+        Type attrType = typeof(AssemblyDescriptionAttribute);
+        object[] attrs = a.GetCustomAttributes(attrType, false);
+
+        if (attrs.Length > 0)
+        {
+            AssemblyDescriptionAttribute desc = (AssemblyDescriptionAttribute)attrs[0];
+            Console.WriteLine("Description is: {0}", desc.Description);
+        }
+        Console.ReadKey();
+    }
+}
